48f58867885fd80826fdb82a4dae01b488955d85
franchuguti <franchuguti22@gmail.com>
2025-01-27 17:29:28 +0000

    camnbios

---
+++ assets/unica.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta lang="es">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow, max-snippet:25, noimageindex, unavailable_after: 2033-09-27">
+
+        <title> Aquí va el título | ejemplo.com </title>
+        <meta property="og:title" content="Aquí va el título | ejemplo.com " />
+        <meta property="twitter:title" content="Aquí va el título | ejemplo.com " />
+        
+        <meta name="description" content="Se te ha olvidado cerrar la etiqueta /footer :)">
+        <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
+        <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
+        
+        <meta property="og:url" content="http://ejemplo.test/" />
+        <meta property="twiitter:url" content="http://ejemplo.test/" />
+
+        <meta property="og:image" content="/imagenes/nike.png" />
+        <meta property="og:image:secure_url" content="/imagenes/nike.png" />
+        <meta property="og:image:alt" content="paisaje de alt" />
+        <meta property="twitter:image" content="/imagenes/nike.png"/>
+        <meta http-equiv="refresh" conten="30; url=https://google.com">
+
+
+        <meta property="og:type" content="website" />
+        <meta property="twitter:card" content="sumary_large_image" />
+        <meta property="twitter:site" content="Fran Perez" />
+        <meta property="twitter:creator" content="Fran Perez" />
+        <!--indexifembeded --Indexa si esta dentro de un iframe sin que el dominio sea indexable--> 
+         <!-- <meta name="rating" content="adult">rta label weebsite -->
+
+<!-- Twitter -->
+<!--<meta name="robots" content="all" -->
+<!--<meta name="robots" content="index,nofollow"> --> 
+<!--<meta name="robots" content="noindex,follow"> --> 
+<!--<meta name="robots" content="max-snippet:0"> Si es 0 es nada pero cualquier numero determina el maximo de caracteres para la meta description --> 
+<!--<meta name="robots" content="none"> No indexable y no follow --> 
+<!--<meta name="robots" content="max-image-preview"> Fija el tamano maximo de la previa de la imagen-->
+<!--<meta name="robots" content="notranslate"> No ofrece traduccion de la pagina een los resultados de busqueda --> 
+<!--<meta name="robots" content="noimageindex"> Para que google no indexe las imagenes para el resultado de busqueda, las imagenes de CSS no se indexan -->
+<!--<meta name="robots" content="unavailable_after: (date)"> No se muestra esta página en los resultados de búsqueda después de la fecha y hora especificadas. Eventos con fecha de caducidad -->
+<!--<meta name="robots" content="nopagereadaloud"> No deja que Google lea por voz la pagina -->
+<!-- User agents Google,Bing, etc para un motor de busqueda especifico-->
+ <!-- <meta name="rating" content="adult">rta label weebsite -->
+    </head>
+<body>
+    <header>
+        <nav></nav>
+    </header>
+        <h1>Página única</h1>
+        <span data-nosnippet>Este atributo hace que un elemento no salga en los motores de busqueda</span>
+    <footer>
+    </footer>
+</body>
