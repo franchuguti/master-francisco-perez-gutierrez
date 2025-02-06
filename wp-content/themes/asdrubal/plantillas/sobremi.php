1058658d827c82139f1171a58ba8b085b70b7193
franchuguti <franchuguti22@gmail.com>
2025-02-06 11:22:36 +0000

    Update sobremi.php

--- wp-content/themes/asdrubal/plantillas/sobremi.php
+++ wp-content/themes/asdrubal/plantillas/sobremi.php
@@ -6,9 +6,12 @@
 <div class="sobre mi"> <h1 class="fran">Sobre mi </h1> </div>
 <div class="selfie">
     <img src="/wp-content/themes/asdrubal/images/arcane.jpg" alt="Fotografía sobre mi">
-    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, nemo harum sequi iusto laboriosam alias sit, voluptate voluptatem quibusdam quaerat aliquid dolores asperiores, odit adipisci odio quas dolore veniam beatae. </p>
+    <p> <?php echo the_content();?> </p>
 </div>
-<?php
+
+<div class="footer">    
+       <?php
     //include_once __DIR__ . ()'/../footer.php');
     get_footer();
 ?> 
+</div>   
