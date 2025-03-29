590d862d957103f8b3f38f40fffee9dfe4b0f3bc
franchuguti <franchuguti22@gmail.com>
2025-03-29 10:20:12 +0000

    front page

---
+++ front-page.php
@@ -0,0 +1,204 @@
+<?php include_once 'components/header.php'; ?>
+
+        <!-- # site main content
+        ================================================== -->
+        <section id="content" class="s-content">
+            <!-- intro
+            ----------------------------------------------- -->
+            <section id="intro" class="s-intro">
+
+                <div class="row s-intro__content width-sixteen-col">
+
+                    <div class="column lg-12 s-intro__content-inner grid-block">
+                        
+                        <div class="s-intro__content-text">
+                            <div class="s-intro__content-pretitle text-pretitle">Hola, soy Francisco,</div>
+                            <h1 class="s-intro__content-title">
+                            SEO Técnico <br>
+                            con experiencia <br>
+                            en UX/UI.  <br>
+                            
+                            </h1>
+                        </div> <!-- s-intro__content-text -->
+
+                        <div class="s-intro__content-media"> 
+                            <div class="s-intro__content-media-inner">                               
+                                <img src="images/Francisco-perez-portrait.webp" title="Retrato de Francisco Perez" alt="Retrato de Francisco Perez">
+                                <div class="lines">                               
+                                    <span></span>                                  
+                                </div>
+                            </div>
+                        </div> <!-- s-intro__content-media -->                      
+
+                        <div class="s-intro__scroll-down">
+                            <a href="#sobremi" class="smoothscroll">
+                                <div class="scroll-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36" fill="none" stroke="#97b34a" stroke-width="1" stroke-linecap="round" stroke-linejoin="round">&lt;!--!  Atomicons Free 1.00 by @atisalab License - https://atomicons.com/license/ (Icons: CC BY 4.0) Copyright 2021 Atomicons --&gt;<polyline points="7 13 12 18 17 13"></polyline><line x1="12" y1="18" x2="12" y2="6"></line></svg>
+                                </div>
+                                <span>Scroll para ver más</span>
+                            </a>
+                        </div> <!-- s-intro__scroll-down -->
+                        
+                    </div> <!-- s-intro__content-inner -->
+
+                </div> <!-- s-intro__content -->
+
+            </section> <!-- end s-intro -->
+
+            <!-- about
+            ----------------------------------------------- -->
+            <section id="sobremi" class="s-about target-section">
+
+                <div class="row s-about__content width-sixteen-col">
+                    <div class="column grid-block grid-section-split">
+
+                        <img src="images/geometric_shape.svg" alt="" class="s-about__content-imgbg">
+
+                        <div class="section-header grid-section-split__header">
+                            <div class="text-pretitle">Sobre mi</div>                           
+                            <h2 class="text-display-title">
+                                Impulsando marcas a través de la búsqueda.
+                            </h2>                           
+                        </div> <!-- end section-header -->
+
+                        <div class="s-about__content-main grid-section-split__primary">
+                            <p class="attention-getter">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Repellendus iste ipsam quod repellat. Hic tempora ullam aperiam 
+                            ipsum optio magni vel inventore voluptatibus nisi maiores laboriosam 
+                            fuga iure, velit eligendi ab vero minima? Quae ducimus ab dignissimos 
+                            iure, eos consequatur est deleniti cum id aliquid neque.
+                            </p>
+
+                            <p class="attention-getter">
+                            Autem tenetur commodi maiores. Lorem ipsum dolor sit, amet consectetur adipisicing elit.  
+                            Est eligendi fugit, facilis velit reiciendis iure laudantium. Praesentium repellat 
+                            corrupti dolor sit sint obcaecati. Modi aut quo molestiae a explicabo maiores necessitatibus 
+                            nemo repellendus architecto? Corrupti numquam ullam nostrum, eveniet at doloribus blanditiis 
+                            aliquid a est porro aspernatur pariatur culpa soluta nesciunt odio quasi maxime debitis illum.
+                            </p>
+
+                            <p class="attention-getter">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Repellendus iste ipsam quod repellat. Hic tempora ullam aperiam 
+                            ipsum optio magni vel inventore voluptatibus nisi maiores laboriosam 
+                            fuga iure, velit eligendi ab vero minima? Quae ducimus ab dignissimos 
+                            iure, eos consequatur est deleniti cum id aliquid neque.
+                            </p>
+                        </div> <!-- end s-about__content-main -->
+
+                        <div class="s-about__content-btn grid-section-split__bottom">
+                            <a href="sobremi.php" class="btn btn--stroke u-fullwidth">Descubre más sobre mi</a>
+
+                            <ul class="s-about__social social-list">
+                                <li>
+                                    <a href="https://www.facebook.com/" target="_blank">
+                                        <svg xmlns="http://www.w3.org/2000/svg" title="Linkedin Icon" alt="Linkedin Icon" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);"><path d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z"></path></svg>
+                                        <span class="u-screen-reader-text">Linkedin</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.facebook.com/" target="_blank">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" title="Instagram Icon" alt="Instagram Icon" style="fill:rgba(0, 0, 0, 1);"><path d="M11.999,7.377c-2.554,0-4.623,2.07-4.623,4.623c0,2.554,2.069,4.624,4.623,4.624c2.552,0,4.623-2.07,4.623-4.624 C16.622,9.447,14.551,7.377,11.999,7.377L11.999,7.377z M11.999,15.004c-1.659,0-3.004-1.345-3.004-3.003 c0-1.659,1.345-3.003,3.004-3.003s3.002,1.344,3.002,3.003C15.001,13.659,13.658,15.004,11.999,15.004L11.999,15.004z"></path><circle cx="16.806" cy="7.207" r="1.078"></circle><path d="M20.533,6.111c-0.469-1.209-1.424-2.165-2.633-2.632c-0.699-0.263-1.438-0.404-2.186-0.42 c-0.963-0.042-1.268-0.054-3.71-0.054s-2.755,0-3.71,0.054C7.548,3.074,6.809,3.215,6.11,3.479C4.9,3.946,3.945,4.902,3.477,6.111 c-0.263,0.7-0.404,1.438-0.419,2.186c-0.043,0.962-0.056,1.267-0.056,3.71c0,2.442,0,2.753,0.056,3.71 c0.015,0.748,0.156,1.486,0.419,2.187c0.469,1.208,1.424,2.164,2.634,2.632c0.696,0.272,1.435,0.426,2.185,0.45 c0.963,0.042,1.268,0.055,3.71,0.055s2.755,0,3.71-0.055c0.747-0.015,1.486-0.157,2.186-0.419c1.209-0.469,2.164-1.424,2.633-2.633 c0.263-0.7,0.404-1.438,0.419-2.186c0.043-0.962,0.056-1.267,0.056-3.71s0-2.753-0.056-3.71C20.941,7.57,20.801,6.819,20.533,6.111z M19.315,15.643c-0.007,0.576-0.111,1.147-0.311,1.688c-0.305,0.787-0.926,1.409-1.712,1.711c-0.535,0.199-1.099,0.303-1.67,0.311 c-0.95,0.044-1.218,0.055-3.654,0.055c-2.438,0-2.687,0-3.655-0.055c-0.569-0.007-1.135-0.112-1.669-0.311 c-0.789-0.301-1.414-0.923-1.719-1.711c-0.196-0.534-0.302-1.099-0.311-1.669c-0.043-0.95-0.053-1.218-0.053-3.654 c0-2.437,0-2.686,0.053-3.655c0.007-0.576,0.111-1.146,0.311-1.687c0.305-0.789,0.93-1.41,1.719-1.712 c0.534-0.198,1.1-0.303,1.669-0.311c0.951-0.043,1.218-0.055,3.655-0.055c2.437,0,2.687,0,3.654,0.055 c0.571,0.007,1.135,0.112,1.67,0.311c0.786,0.303,1.407,0.925,1.712,1.712c0.196,0.534,0.302,1.099,0.311,1.669 c0.043,0.951,0.054,1.218,0.054,3.655c0,2.436,0,2.698-0.043,3.654H19.315z"></path></svg>
+                                        <span class="u-screen-reader-text">Instagram</span>
+                                    </a>
+                                </li>
+                            </ul> <!-- end s-footer__social -->
+                        </div> <!-- end s-about__content-btn -->
+                    
+                    </div> <!-- content-inner -->
+                </div>              
+            </section> <!-- end s-about -->
+            <!-- expertise
+            ----------------------------------------------- -->
+            <section id="expertise" class="s-expertise">
+
+                <div class="row s-expertise__content width-sixteen-col">
+                    <div class="column xl-12 grid-block grid-section-split">
+
+                        <div class="section-header grid-section-split__header">
+                            <div class="text-pretitle">Especialización</div>                           
+                            <h2 class="text-display-title">
+                            Mis principales áreas de especialización.
+                            </h2>     
+                            
+                            <p class="lead">
+                            El <Strong>SEO</Strong> ya no es solo posicionamiento en buscadores, 
+                            sino una combinación de estrategia técnica, experiencia de usuario y optimización de contenido. 
+                            Como especialista en <Strong>SEO técnico</Strong> con formación en <Strong>UX</Strong> y <Strong>UI</Strong>, 
+                            ayudo a empresas y negocios online a mejorar su visibilidad,
+                            optimizar la experiencia de sus usuarios y aumentar conversiones.
+                            </p>
+                        </div> <!-- end section-header -->
+
+                        <div class="s-expertise__content-main grid-section-split__primary">
+
+                            <div class="grid-list-items list-items show-ctr">
+                                <div class="grid-list-items__item list-items__item">
+                                    <div class="grid-list-items__title list-items__item-header">
+                                        <h3 class="list-items__item-title">🚀 Optimización profunda para un sitio web rápido, rastreable y bien posicionado.</h3>
+                                    </div>
+                                    <div class="grid-list-items__text list-items__item-text">
+                                        <p>
+                                        El SEO técnico es la base de cualquier estrategia de posicionamiento. 
+                                        Analizo y optimizo la estructura de tu sitio para mejorar la indexación, 
+                                        la velocidad de carga y la experiencia del usuario. Haz que tu web sea más visible y eficiente desde la base.
+                                        </p>
+                                    </div>
+                                </div> <!-- list-item -->
+                                <div class="grid-list-items__item list-items__item">
+                                    <div class="grid-list-items__title list-items__item-header">
+                                        <h3 class="list-items__item-title">🎨 Cuando SEO y UX trabajan juntos, los resultados se multiplican.</h3>
+                                    </div>
+                                    <div class="grid-list-items__text list-items__item-text">
+                                        <p>
+                                        El posicionamiento web no solo depende del contenido y los enlaces, sino también de una experiencia 
+                                        de usuario fluida y una interfaz optimizada para la conversión. Te ayudo a mejorar la estructura, 
+                                        navegación y usabilidad de tu sitio para atraer y retener tráfico de calidad.
+                                        </p>
+                                    </div>
+                                </div> <!-- list-expertise__item -->
+                                <div class="grid-list-items__item list-items__item">
+                                    <div class="grid-list-items__title list-items__item-header">
+                                        <h3 class="list-items__item-title">🛍️ Más tráfico, más conversiones, más ventas.</h3>
+                                    </div>
+                                    <div class="grid-list-items__text list-items__item-text">
+                                        <p>
+                                        Si tienes un eCommerce o un sitio basado en contenido, necesitas
+                                         algo más que palabras clave: una estrategia SEO enfocada en resultados. 
+                                         Optimizo tu tienda online o blog para mejorar su visibilidad en buscadores y maximizar conversiones.
+                                        </p>
+                                    </div>
+                                </div> <!-- list-expertise__item -->
+                                <div class="grid-list-items__item list-items__item">
+                                    <div class="grid-list-items__title list-items__item-header">
+                                        <h3 class="list-items__item-title">📈 Estrategia SEO personalizada para empresas y equipos.</h3>
+                                    </div>
+                                    <div class="grid-list-items__text list-items__item-text">
+                                        <p>
+                                        Ya sea que necesites una hoja de ruta para mejorar tu SEO o formación para tu equipo, 
+                                        te ayudo a implementar estrategias efectivas y sostenibles.
+                                        </p>
+                                    </div>
+                                </div> <!-- list-expertise__item -->
+
+                            </div> <!-- grid-list-items -->
+                        </div> <!-- end s-expertise__content-main -->
+
+                        <div class="s-expertise__content-btn grid-section-split__bottom">
+                            <a href="services.php" class="btn btn--stroke u-fullwidth">View All Services</a>
+                        </div> <!-- end s-about__content-btn -->
+
+                    </div> <!-- end grid-block-->
+                </div> <!-- end s-expertise__content -->
+
+            </section> <!-- end s-expertise -->
+
+        </section> <!-- s-content-->
+        <!-- # cta
+        ================================================== -->
+        <section id="cta" class="s-cta">
+             <?php include_once __DIR__ . '/components/cta.php'; ?>       
+        </section> <!-- end s-cta -->
+        <?php include_once __DIR__ . '/components/articulos.php'; ?>   
+<?php include_once __DIR__ . '/components/footer.php'; ?>
