c992a6f439fd0aff3edf24e6e6bcd7f0157d996b
franchuguti <franchuguti22@gmail.com>
2025-05-08 07:29:09 +0100

    Borrar el Sitemap por defecto de Wordpress
    Borrar el Sitemap por defecto de Wordpress

--- wp-content/themes/asdrubal/functions.php
+++ wp-content/themes/asdrubal/functions.php
@@ -8,4 +8,9 @@ function tresposts (){
 }
 add_shortcode('latest_post' , 'tresposts');
 
+// Borrar Sitemap por defecto
+add_filter( 'wp_sitemaps_enabled', '__return_false' );
+if (has_action('init', 'wp_sitemaps_get_server')){
+remove_action('init', 'wp_sitemaps_get_server');
+}
 ?>
