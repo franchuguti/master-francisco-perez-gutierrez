a695ba6819a3b1783d3abebc9af9c9679081581d
franchuguti <franchuguti22@gmail.com>
2025-05-20 07:53:53 +0100

    Eliminado MailCHimp

--- wp-content/themes/franperez/js/main.js
+++ wp-content/themes/franperez/js/main.js
@@ -3,14 +3,6 @@
 
     'use strict';
 
-    const cfg = {
-
-        // MailChimp URL
-        mailChimpURL : 'https://facebook.us1.list-manage.com/subscribe/post?u=1abf75f6981256963a47d197a&amp;id=37c6d8f4d6' 
-
-    };
-
-
    /* preloader
     * -------------------------------------------------- */
     const ssPreloader = function() {
